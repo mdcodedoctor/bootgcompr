c56bd74143b6270b443c8ba3b62b06ddfa0612a6
mdcodedoctor <150513830+mdcodedoctor@users.noreply.github.com>
2025-10-09 21:37:37 +0200

    Added method argument to gcomp wrapper

--- R/gcomp_boot.R
+++ R/gcomp_boot.R
@@ -7,9 +7,9 @@
 #' @export
 
 # Wrapper function for gcomp_boot_func
-gcomp_boot <- function(adj.vars = NULL, R = 1000) {
+gcomp_boot <- function(adj.vars = NULL, R = 1000, method = NULL) {
   # Return a partially applied function
   function(data, variable, by, ...) {
-    bootgcompr::gcomp_boot_func(data, variable, by, adj.vars = adj.vars, R = R, ...)
+    bootgcompr::gcomp_boot_func(data, variable, by, adj.vars = adj.vars, R = R, method = method, ...)
   }
 }
